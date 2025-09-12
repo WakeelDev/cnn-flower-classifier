071acc258b305a822824f1d0a3be2c7daf65dc1b
WakeelDev <your_github_wakilkhn4697@gmail.com>
2025-09-12 17:17:11 +0500

    Fix: switched to tensorflow-cpu and updated keras imports

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.applications import MobileNetV2
