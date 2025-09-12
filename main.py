19d0fb7b4abb80fb8306bd9487ac99bc8394b18e
Wakeel Khan <wakilkhn4697@gmail.com>
2025-09-12 17:19:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import json
 import numpy as np
-from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.applications import MobileNetV2
